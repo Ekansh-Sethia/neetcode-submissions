5c996e6f9f54ce227ba4ab021c7e99f20b87e182
Ekansh Sethia <ekanshjain135@gmail.com>
2026-06-24 23:42:45 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int solve(vector<int>& nums, int ind, vector<int>& dp){
+        if(ind == 0) return nums[0];
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+
+        int pick = nums[ind] + solve(nums,ind-2, dp);
+        int notPick = solve(nums,ind-1, dp);
+
+        return dp[ind] = max(pick,notPick);
+    }
+    int tabulation(vector<int> &nums, int n){
+        vector<int> dp(n,0);
+        dp[0] = nums[0];
+
+        for(int i = 1; i < n; i++){
+            int pick = nums[i];
+            if(i >= 2) pick += dp[i-2];
+            
+            int notPick = dp[i-1];
+
+            dp[i] = max(pick,notPick);
+        }
+        return dp[n-1];
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        return tabulation(nums,n);
+    }
+};
