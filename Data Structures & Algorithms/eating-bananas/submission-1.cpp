c5983b29c40d27413fd71e0cb43f40d766d29a17
Ekansh Sethia <ekanshjain135@gmail.com>
2026-07-02 00:24:31 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    bool possibleWithinH(vector<int>& piles, int k, int h){
+        int tot = 0;
+        for(int i : piles){
+            tot += (i + k - 1) / k;
+        }
+        return tot <= h;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxi = -1;
+        for(int i : piles) maxi = max(maxi,i);
+        int st = 1;
+        int end = maxi;
+        int ans = maxi;
+
+        while(st <= end){
+            int mid = st + (end-st)/2;
+
+            if(possibleWithinH(piles,mid,h)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else st = mid+1;
+        }
+
+        return ans;
+    }
+};
